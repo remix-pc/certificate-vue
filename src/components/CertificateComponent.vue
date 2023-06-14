79702e393014adf9eff313da82c4a3afe61b377c
remix-pc <guimoreiramanow.contato@gmail.com>
2023-06-14 17:10:26 -0300

    preparation for the first component

---
+++ src/components/CertificateComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="category">
+        <h1>{{ categoryName }}</h1>
+        <div class="certificate">
+            <h4>{{ certificateName }}</h4>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CertificateComponent',
+
+    props:{
+        categoryName: String,
+        certificateName: String,
+    }
+
+}
+</script>
